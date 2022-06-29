ece059f145ab40b36e8591f4aaf0fc67c86dd888
camila suzuki <camila.suzuki.p@gmail.com>
2022-06-29 12:51:28 -0300

    fix:not found messages

--- animals/views.py
+++ animals/views.py
@@ -10,41 +10,41 @@ class AnimalView(APIView):
 
     def patch(self,request,animal_id=None):
         if animal_id:
-            animal = get_object_or_404(Animal,pk=animal_id)
+            try:
+              animal = Animal.objects.get(pk=animal_id)
+            except Animal.DoesNotExist:
+                return Response({"message":"Animal not found"},status=status.HTTP_404_NOT_FOUND)  
             serializer = AnimalSerializer(animal,request.data,partial=True)
             serializer.is_valid(raise_exception=True)
             try:
               serializer.save()
             except KeyError as e:
-              return Response(f'You can not update {e} property',status.HTTP_400_BAD_REQUEST)
+              return Response({"message":f'You can not update {e} property'},status.HTTP_400_BAD_REQUEST)
 
             return Response(serializer.data)
 
     def delete(self,request,animal_id=None):
         if animal_id:
-            animal = get_object_or_404(Animal,pk=animal_id)
-            animal.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+          try:
+              animal = Animal.objects.get(pk=animal_id)
+          except Animal.DoesNotExist:
+                return Response({"message":"Animal not found"},status=status.HTTP_404_NOT_FOUND)
+          animal.delete()
+          return Response(status=status.HTTP_204_NO_CONTENT)
 
     def get(self,request,animal_id=None):
 
         if animal_id:
-            animal = Animal.objects.get(pk=animal_id)
-            serializer = AnimalSerializer(animal)
-            return Response(serializer.data)
+          try:
+              animal = Animal.objects.get(pk=animal_id)
+          except Animal.DoesNotExist:
+                return Response({"message":"Animal not found"},status=status.HTTP_404_NOT_FOUND)
+          animal = Animal.objects.get(pk=animal_id)
+          serializer = AnimalSerializer(animal)
+          return Response(serializer.data)
        
-
         animals = Animal.objects.all()
-        print(animals)
         serializer = AnimalSerializer(animals,many=True)
-        # try:
-        #     print(serializer.data)
-        # except Exception as e:
-        #     print(e)
-
-
- 
-
         return Response(serializer.data)
 
     def post(self,request):
