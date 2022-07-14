c01480845f92d14f73e5a0106cc9e498970e7d7d
camila suzuki <camila.suzuki.p@gmail.com>
2022-07-14 13:17:31 -0300

    a

--- kenziePet/settings.py
+++ kenziePet/settings.py
@@ -10,6 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+from pathlib import Path
+
+import django_on_heroku
+
+# import dj_database_url
+import dotenv
+
+dotenv.load_dotenv()
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +30,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n27pngqn9z0v$gx0w*bwga^pz_df80@+(axpv_l(em@27kfgqc'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+CSRF_TRUSTED_ORIGINS = ["https://heroku-postgres-kenzie-pet.herokuapp.com/"]
 
 ALLOWED_HOSTS = []
 
@@ -118,10 +130,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_on_heroku.settings(locals())
